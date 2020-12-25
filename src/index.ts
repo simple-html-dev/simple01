057c729d13daa5ceda8362339402c403e60c9802
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-12-25 16:08:25 +0100

    chore: test

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { customElement } from "@simple-html/core";
+import { html } from "lit-html";
+
+@customElement("app-root")
+export default class extends HTMLElement {
+  render() {
+    return html`<span>awdssddsssssddsw<span></span></span>`;
+  }
+}
