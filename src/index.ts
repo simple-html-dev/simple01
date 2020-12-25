7e5a237b5b154a82caf5b648f2427102e1e6ae70
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-12-25 22:48:43 +0100

    chore: test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "./index.css";
 
 // only use hmr if development
-if (process.env.NODE_ENV === "development") {
+/* if (process.env.NODE_ENV === "development") {
   const { applyPolyfill } = require("custom-elements-hmr-polyfill");
   applyPolyfill();
-}
+} */
 
 // load element, so we are sure we have latest before setting root
 import("./app-root").then(() => {
