304e0ca1d939700b181513eb27ea0dce79f11484
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-12-25 15:01:34 +0000

    init test

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {customElement} from "@simple-html/core"
+import {html} from 'lit-html'
+
+@customElement("app-root")
+export default class extends HTMLElement {
+
+  render(){
+    return html`<span>awdssddsssssddsw<span>`
+  }
+
+}
+
+
